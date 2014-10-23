a0927903750a00091e84d0241ca38f4c9e6b4370
Shehovtsov Artem <a.shehovtsov@adriver.ru>
2014-10-23 11:56:56 +0400

    Edge equality realization

--- calc/graph/Edge.py
+++ calc/graph/Edge.py
@@ -4,4 +4,13 @@ from Vertice import Vertice
 class Edge():
 
     def __init__(self, v1=Vertice(), v2=Vertice()):
-        self.vertices = (v1,v2)
+        self.vertices = (v1, v2)
+
+    def __eq__(self, other):
+        """
+        The two edges (u, v) and (v, u) are the same.
+        In other words, the pair is not ordered.
+        :param other: Edge
+        :return: bool
+        """
+        return set(self.vertices) == set(other.vertices)
