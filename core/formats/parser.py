eea09ddd774e7f3c48cac7b7f2d0df003747684e
cehmja <shekhovtsov@tut.by>
2014-10-25 13:22:38 +0400

    add common structure for parser various formats

---
+++ core/formats/parser.py
@@ -0,0 +1,9 @@
+class Parser():
+    def __init__(self):
+        pass
+
+    def encode(self):
+        raise NotImplementedError
+
+    def decode(self):
+        raise NotImplementedError
--- core/formats/smiles.py
+++ core/formats/smiles.py
@@ -12,29 +12,40 @@ atom : '[' <mass> symbol <chiral> <hcount> <sign<charge>> ']'
 import re
 
 from core.exception.exception import BadFormatError
+from parser import Parser
+
 
 SMILES_STRUCTURE = re.compile('^[A-Za-z0-9\[\]\-\=\#\:]*$')
 
 
-def is_smiles(string):
-    if not re.match(SMILES_STRUCTURE, string):
-        raise BadFormatError
-    return True
+class SmilesParser(Parser):
+    def __init__(self):
+        pass
+
+    def is_smiles(self, string):
+        """
+        Check if it string can be SMILES String or not
+        :param string:
+        :return:
+        """
+        if not re.match(SMILES_STRUCTURE, string):
+            raise BadFormatError
+        return True
 
 
-def decode(string=''):
-    is_smiles(string)
-    print(string)
-    return None
+    def decode(self, string=''):
+        self.is_smiles(string)
+        print(string)
+        return None
 
 
-def encode(molecule):
-    return None
+    def encode(self, molecule):
+        return None
 
 
-def get_canonical():
-    return encode(decode(''))
+    def get_canonical(self):
+        return self.encode(self.decode(''))
 
 
 if __name__ == '__main__':
-    decode('[c]=c')
+    p = SmilesParser()
