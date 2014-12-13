a10062e6045d27e46dd6aa0d276220a6a568e398
cehmja <shekhovtsov@tut.by>
2014-12-13 22:13:51 +0300

    Molecular mas definition

--- structure/Molecule.py
+++ structure/Molecule.py
@@ -6,5 +6,8 @@ class Molecule():
         self.bonds = set()
         self.molecular_graph = Graph.Graph.from_molecule(self.atoms, self.bonds)
 
+    def molecular_mass(self):
+        return sum([atom.get_relative_mass() for atom in self.atoms])
+
 if __name__ == '__main__':
     print Molecule()
