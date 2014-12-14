36750220f20dc56f31dac17bb50bc68adaff13a8
cehmja <shekhovtsov@tut.by>
2014-12-14 23:59:00 +0300

    Adding cis/trans argument in bond

--- structure/Bond.py
+++ structure/Bond.py
@@ -1,7 +1,8 @@
 from structure.Atom import Atom
 class Bond():
-    def __init__(self, order=1, atom1=None, atom2=None):
+    def __init__(self, order=1, atom1=None, atom2=None, cis_trans=None):
         self.order = order
+        self.cis_trans = cis_trans
         self.atoms = set([atom1, atom2])
 
 
