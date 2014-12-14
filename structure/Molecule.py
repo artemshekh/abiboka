3ff59f827272822e110073084fdf932cd00a3fe8
cehmja <shekhovtsov@tut.by>
2014-12-15 00:00:25 +0300

    Add method molecular_graph for molecule

--- structure/Molecule.py
+++ structure/Molecule.py
@@ -4,10 +4,12 @@ class Molecule():
     def __init__(self):
         self.atoms = set()
         self.bonds = set()
-        self.molecular_graph = Graph.Graph.from_molecule(self.atoms, self.bonds)
 
     def molecular_mass(self):
         return sum([atom.get_relative_mass() for atom in self.atoms])
+
+    def molecular_graph(self):
+        return Graph.Graph.from_molecule(self.atoms, self.bonds)
 
 if __name__ == '__main__':
     print Molecule()
