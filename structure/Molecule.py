a9a930b8f13a956a6380df38d5947cbba9111716
cehmja <shekhovtsov@tut.by>
2014-12-17 01:41:44 +0300

    methods add_atoms, add_bonds molecule class

--- structure/Molecule.py
+++ structure/Molecule.py
@@ -8,8 +8,16 @@ class Molecule():
     def add_atom(self, atom):
         self.atoms.add(atom)
 
+    def add_atoms(self, atoms):
+        for atom in atoms:
+            self.add_atom(atom)
+
     def add_bond(self, bond):
         self.bonds.add(bond)
+
+    def add_bonds(self, bonds):
+        for bond in bonds:
+            self.add_bond(bond)
 
     def molecular_mass(self):
         return sum([atom.get_relative_mass() for atom in self.atoms])
