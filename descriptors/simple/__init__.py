5b2ac180d31690863ed99f8b821fb1eeb513e719
cehmja <shekhovtsov@tut.by>
2014-12-20 22:15:31 +0300

    Descriptor: sum of atomic van der Waals volumes (scaled on Carbon atom)

--- descriptors/simple/__init__.py
+++ descriptors/simple/__init__.py
@@ -3,7 +3,12 @@
 Simple descriptor of molecule
 
 """
+from utils.periodic_table import periodic_table
+import math
 
+
+def sv(molecule):
+    return sum([(4 * math.pi * periodic_table[atom.Z]['vdw_radius']**3)/3 for atom in molecule.atoms])/((4 * math.pi * periodic_table[6]['vdw_radius']**3)/3)
 
 
 _ = {
@@ -12,5 +17,7 @@ _ = {
     "MW": lambda x: x.molecular_mass(),
 
     # average molecular weight
-    "AMW": lambda x: x.molecular_mass()/len(x.atoms)
+    "AMW": lambda x: x.molecular_mass()/len(x.atoms),
+
+    "SV": sv
 }
