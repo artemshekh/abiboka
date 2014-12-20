e5fbd3c3e32ce3e72b7aabb4205660f2872ca30e
cehmja <shekhovtsov@tut.by>
2014-12-20 19:31:05 +0300

    Average molecular weight descriptor

--- descriptors/simple/__init__.py
+++ descriptors/simple/__init__.py
@@ -7,8 +7,10 @@ Simple descriptor of molecule
 
 
 _ = {
-    """
-    molecular weight
-    """
-    "MW": lambda x: x.molecular_mass()
+
+    #molecular weight
+    "MW": lambda x: x.molecular_mass(),
+
+    # average molecular weight
+    "AMW": lambda x: x.molecular_mass()/len(x.atoms)
 }
