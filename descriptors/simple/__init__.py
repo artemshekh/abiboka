bf28ca5a5700bdc6c4fbf603151ca776ef754015
cehmja <shekhovtsov@tut.by>
2014-12-22 21:50:46 +0300

    SP sum of atomic polarizabilities (scaled on Carbon atom)

--- descriptors/simple/__init__.py
+++ descriptors/simple/__init__.py
@@ -10,6 +10,8 @@ import math
 def sv(molecule):
     return sum([(4 * math.pi * periodic_table[atom.Z]['vdw_radius']**3)/3 for atom in molecule.atoms])/((4 * math.pi * periodic_table[6]['vdw_radius']**3)/3)
 
+def sp(molecule):
+    return sum([periodic_table[atom.Z]['atomic_polarizability'] for atom in molecule.atoms])/periodic_table[6]['atomic_polarizability']
 
 _ = {
 
@@ -19,5 +21,7 @@ _ = {
     # average molecular weight
     "AMW": lambda x: x.molecular_mass()/len(x.atoms),
 
-    "SV": sv
+    "SV": sv,
+
+    "SP": sp
 }
