5e85e6ed0be7b61fe2dd57ae163fac3085b2f6ff
cehmja <shekhovtsov@tut.by>
2014-12-24 22:36:36 +0300

    Depth-first search for atoms in molecule

--- calc/graph/__init__.py
+++ calc/graph/__init__.py
@@ -2,3 +2,15 @@ __author__ = 'a.shehovtsov'
 """
 
 """
+
+def dfs(molecule):
+    ex = set()
+    def dfs_(atom):
+        ex.add(atom)
+        for bond in atom.bonds:
+            for atom_ in bond:
+                if atom_ not in ex:
+                    dfs_(atom_)
+    for atom in molecule.atoms:
+        dfs_(atom)
+    print len(ex), len(molecule.atoms)
