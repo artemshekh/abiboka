8424d2cc339d731770edafa2a9f768fbb4448476
cehmja <shekhovtsov@tut.by>
2014-12-26 00:25:55 +0300

    check_cycle function

--- calc/graph/__init__.py
+++ calc/graph/__init__.py
@@ -14,3 +14,34 @@ def dfs(molecule):
     for atom in molecule.atoms:
         dfs_(atom)
     print len(ex), len(molecule.atoms)
+
+def check_cycle(molecule):
+
+    def cycle(parent, atom):
+        """
+        IT's very slow and unefficient check either atom in cycle or not
+        for _ in molecule.atoms:
+            visited, cycle = [], [False]
+            cycle_(_, _)
+        :param parent:
+        :param atom:
+        :return: set of atoms from molecule that situated in cycle
+        """
+        visited.append(atom)
+        next_iteration_atoms = set()
+        for bond in atom.bonds:
+            for atom_ in bond:
+                if atom_ is not parent:
+                    if atom_ is visited[0]:
+                        cycle_[0] = True
+                    if atom_ not in visited:
+                        next_iteration_atoms.add(atom_)
+        for next_atom in next_iteration_atoms:
+                cycle(atom, next_atom)
+    s = set()
+    for _ in molecule.atoms:
+        visited, cycle_ = [], [False]
+        cycle(_, _)
+        if cycle_[0]:
+            s.add(_)
+    return s
