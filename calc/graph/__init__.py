a9df8f65324687b8bcfbed74b0e4c78257b54d14
cehmja <shekhovtsov@tut.by>
2014-12-27 19:56:21 +0300

    update dfs

--- calc/graph/__init__.py
+++ calc/graph/__init__.py
@@ -3,17 +3,12 @@ __author__ = 'a.shehovtsov'
 
 """
 
-def dfs(molecule):
-    ex = set()
-    def dfs_(atom):
-        ex.add(atom)
-        for bond in atom.bonds:
-            for atom_ in bond:
-                if atom_ not in ex:
-                    dfs_(atom_)
-    for atom in molecule.atoms:
-        dfs_(atom)
-    print len(ex), len(molecule.atoms)
+def dfs(atom, ex):
+    ex.add(atom)
+    for bond in atom.bonds:
+        for atom_ in bond:
+            if atom_ not in ex:
+                dfs(atom_, ex)
 
 def check_cycle(molecule):
 
