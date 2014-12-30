81f8bf371ca723e929da4eb18dfaa9be7f70e653
cehmja <shekhovtsov@tut.by>
2014-12-30 20:15:49 +0300

    add version with H first Zagreb Index

--- descriptors/topological.py
+++ descriptors/topological.py
@@ -4,5 +4,8 @@ Topological descriptors
 """
 
 def zm1(molecule):
-    return sum([len(atom.bonds)**2 for atom in molecule.atoms])
+    return sum([len(atom.bonds)**2 for atom in molecule.hydrogen_suppressed().atoms])
+
+def zm1_H(molecule):
+    return sum([len(atom.bonds)**2 for atom in moleculeatoms])
 
