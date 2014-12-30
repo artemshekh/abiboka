5096c00006c8b4fbf402aed85aa5ae3bb9b45e61
cehmja <shekhovtsov@tut.by>
2014-12-30 22:20:44 +0300

    first Zagreb index by valence vertex degrees

--- descriptors/topological.py
+++ descriptors/topological.py
@@ -9,3 +9,43 @@ def zm1(molecule):
 def zm1_H(molecule):
     return sum([len(atom.bonds)**2 for atom in moleculeatoms])
 
+close_shell = [1, 3, 11, 19, 37, 55, 87]
+
+def valence_electrones(atom):
+
+    for period,n in enumerate(close_shell):
+        if atom.Z < n:
+            core_e, valence_e = close_shell[period-1]-1, atom.Z - close_shell[period-1] + 1
+            break
+    if valence_e:
+       return valence_e
+    else:
+        return atom.Z - 87
+
+
+def valence_degree(atom):
+    # all valence electrons of the ith atom
+    vd = valence_electrones(atom)
+    for _ in atom.connected_with():
+        if atom.Z == 1:
+            vd -= 1
+    return vd
+
+def valence_degree_(atom):
+    # the electronic identity of the atom in terms of both valence electron and core electron counts
+    vd = valence_electrones(atom)
+    for _ in atom.connected_with():
+        if atom.Z == 1:
+            vd -= 1
+    vd = float(vd)/(atom.Z - valence_e - 1)
+    return vd
+
+
+def zm1v(molecule):
+    return sum([valence_degree(atom)**2 for atom in molecule.hydrogen_suppressed().atoms])
+
+def zm1v_(molecule):
+    # depends from core electrons
+    return sum([valence_degree(atom)**2 for atom in molecule.hydrogen_suppressed().atoms])
+
+
