9b403f37502b6efac0be57379b42b43da805b59c
cehmja <shekhovtsov@tut.by>
2015-01-01 16:49:54 +0300

    edge degree count vector

--- descriptors/connectivity.py
+++ descriptors/connectivity.py
@@ -5,8 +5,18 @@ Connectivity descriptors
 Edge degree
 
 """
+
+from collections import Counter
+
 from utils.functional import cached
 
 @cached
 def edge_degree(molecule):
     return sum([sum(row) for row in molecule.edge_adjacency_matrix])
+
+@cached
+def edge_degree_count(molecule):
+    counter = Counter()
+    for row in molecule.edge_adjacency_matrix:
+        counter[sum(row)] += 1
+    return counter
