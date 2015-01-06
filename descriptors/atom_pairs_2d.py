21d1902c066b9421f732e4766cb28d378811b616
cehmja <shekhovtsov@tut.by>
2015-01-07 02:08:08 +0300

    Presence/absence pair of atoms at topological distance

--- descriptors/atom_pairs_2d.py
+++ descriptors/atom_pairs_2d.py
@@ -24,3 +24,17 @@ def topological_distance_sum(molecule, atom1, atom2):
     if z1 == z2:
         descriptor = descriptor/2
     return descriptor
+
+def presence_at_topological_distance(molecule, atom1, atom2, distance):
+    z1 = periodic_table_by_symbol[atom1]['z']
+    z2 = periodic_table_by_symbol[atom2]['z']
+    dist_matrix = distance_matrix(molecule)
+    molecule = molecule.hydrogen_suppressed
+    list1 = [index for index, atom in enumerate(molecule.atoms) if atom.Z == z1]
+    list2 = [index for index, atom in enumerate(molecule.atoms) if atom.Z == z2]
+    for i in list1:
+        for j in list2:
+            if i!=j:
+                 if dist_matrix[i][j] == distance:
+                     return True
+    return False
