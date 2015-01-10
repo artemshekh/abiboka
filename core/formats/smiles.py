5d37b6bc68b2a413ed757d1addb6203a634dd5e0
cehmja <shekhovtsov@tut.by>
2015-01-10 12:59:32 +0300

    smiles format refactor

--- core/formats/smiles.py
+++ core/formats/smiles.py
@@ -30,7 +30,7 @@ class SmilesParser(Parser):
                                        '|C|N|O|Cl|Br|F|I|S|P|B|\*|n|o|c|s|p)'
                                        '(?P<bonds>[=#%/()0-9\\\\\.]*)$')
         self.SQUARE_BRACKET = re.compile('\[(?P<mass>[0-9]{0,3})'
-                                         '(?P<symbol>Th|Rh|[a-gi-zA-GI-Z]{1,2}|H|Hf|Ho|Hg)'
+                                         '(?P<symbol>th|rh|[a-gi-zA-GI-Z]{1,2}|h|hf|ho|hg)'
                                          '(?P<chiralsign>@{0,2})'
                                          '(?P<chiralclass>(AL|TB|SP|OH)?[0-9]{0,2})'
                                          '(?P<hpresence>h?|H?)(?P<hcount>[0-9]?)'
@@ -48,160 +48,142 @@ class SmilesParser(Parser):
             raise BadFormatError
         return True
 
-    def decode(self, string=''):
-        # check that our string is really smile
-        self.is_smiles(string)
-
-        smiles_string = string
-
+    def decode(self, string):
         molecule = Molecule()
-        previuos_atom = None
+        numbering_stack = {}
+        previous_atom = None
         previous_bond = None
         atom_stack = []
         after_branch_close = False
         after_branch_close_open = False
-        numbering_stack = {}
+        regexp = re.compile('(?P<atom>C|\[[a-zA-Z0-9\-+@]*\]'
+                                '|C|N|O|Cl|Br|F|I|S|P|B|\*|n|o|c|s|p)'
+                                '(?P<bonds>[=#%/()0-9\\\\\.]*)')
 
-        while len(smiles_string) > 0:
-            index = 0
-            while index < len(smiles_string):
-                is_atom = self.RE_STRUCTURE.match(smiles_string[0:index+1])
-                if is_atom:
-                    atom, smiles_string = smiles_string[0:index+1], smiles_string[index+1:]
-                    while smiles_string and self.RE_STRUCTURE.match(atom + smiles_string[0]):
-                        atom, smiles_string = atom + smiles_string[0], smiles_string[1:]
-                    structure = re.match(self.RE_STRUCTURE, atom).groupdict()
-                    atom, bond_expression = structure['atom'], structure['bonds']
-                    charge = 0
-                    chiral = False
-                    hcount = 0
-                    n = None
-                    bracket = False
-                    if atom.startswith('['):
-                        bracket = True
-                        bracket_atom = self.SQUARE_BRACKET.match(atom).groupdict()
-                        atom = bracket_atom['symbol']
-                        if bracket_atom['charge']:
-                            charge = bracket_atom['charge']
-                        if bracket_atom['chiralsign']:
-                            chiral = bracket_atom['chiralsign']
-                        if bracket_atom['hpresence']:
-                            if not bracket_atom['hcount']:
-                                hcount = 1
-                            else:
-                                hcount = int(bracket_atom['hcount'])
-                        if bracket_atom['mass']:
-                            n = int(bracket_atom['mass'])
-                    if charge == '+':
-                        charge = 1
-                    elif charge == '-':
-                        charge = -1
+        parsing_string = re.findall(regexp, string)
+        for atom_exp, bond_exp in parsing_string:
+
+            #PARSING ATOM EXPRESSION
+            aromatic = False
+            if atom_exp.islower():
+                aromatic = True
+
+            atom_symbol = atom_exp.capitalize()
+            neuthron_number = None
+            charge = 0
+            h_presence = False
+            h_count = 0
+            chiral_sign = ''
+            chiral_class = ''
+            bracket = False
+
+            if atom_symbol[0] == '[':
+                bracket = True
+                complex_atom = self.SQUARE_BRACKET.match(atom_symbol).groupdict()
+                atom_symbol = complex_atom['symbol'].capitalize()
+                neuthron_number = int(complex_atom['mass'] or 0)
+
+                h_presence = True if complex_atom['hpresence'] else False
+                if h_presence:
+                    h_count = int(complex_atom['hcount'] or 1)
+
+                charge_string = complex_atom['charge']
+                if charge_string:
+                    if len(charge_string) == 1:
+                        if charge_string == '-':
+                            charge = -1
+                        elif charge_string == '+':
+                            charge = 1
                     else:
-                        charge = int(charge)
-                    if atom.islower():
-                        aromatic = True
-                    else:
-                        aromatic = False
-                    atom = atom.capitalize()
-                    try:
-                        z = periodic_table_by_symbol[atom]['z']
-                        if z == '*':
-                            z = 6
-                        atom = Atom(z=z, n=n, charge=charge, aromatic=aromatic, chiral=chiral, bracket=bracket)
-
-                        if hcount:
-                            for x in range(hcount):
-                                h = Atom(1)
-                                molecule.atoms.append(h)
-                                bond_h = Bond(order=1)
-                                molecule.bonds.append(bond_h)
-                                bond_h.add_atom(h)
-                                bond_h.add_atom(atom)
-                    except Exception as err:
-                        print err
-                        pass
-                        raise
-                    molecule.atoms.append(atom)
-                    if previuos_atom:
-                        previous_bond.add_atom(atom)
-                        if after_branch_close:
-                            atom_pop = atom_stack.pop()
-                            previous_bond.add_atom(atom_pop)
-                            after_branch_close = False
-                        if after_branch_close_open:
-                            previous_bond.add_atom(atom_stack[-1])
-                            after_branch_close_open = False
+                        if charge_string[0] == '-':
+                            charge = 0 - int(charge_string[1:])
                         else:
-                            previous_bond.add_atom(previuos_atom)
-                        molecule.bonds.append(previous_bond)
-                    previuos_atom = atom
-                    if not bond_expression:
-                        bond = Bond(order=1)
-                    else:
-                        # we have something in bond
-                        # we want splt it on 2 part first part bond with number, other all except it
+                            charge = int(charge_string[1:])
+                chiral_sign = complex_atom['chiralsign']
+                chiral_class = complex_atom['chiralclass']
+            z = periodic_table_by_symbol[atom_symbol]['z']
 
-                        breaking_bond = self.BREAKING_BOND.match(bond_expression)
-                        if breaking_bond:
-                            breaking_bond = breaking_bond.groupdict()
-                            breaking_bond, tail = breaking_bond['breaking_bond'], breaking_bond['tail']
-                            if not breaking_bond[0].isdigit() and breaking_bond[0] != '%':
-                                cis_trans_sign, numbers = breaking_bond[0], breaking_bond[1:]
-                            else:
-                                cis_trans_sign, numbers = None, breaking_bond
-                            numbers_list = []
-                            while numbers:
-                                if not numbers[0] == '%':
-                                    number, numbers = numbers[0], numbers[1:]
-                                else:
-                                    number, numbers = numbers[1:3], numbers[3:]
-                                numbers_list.append(number)
+            # CREATING ATOM
+            atom = Atom(z=z, n=neuthron_number or z, charge=charge, chiral=chiral_class + chiral_sign, aromatic=aromatic, bracket=bracket)
+            molecule.atoms.append(atom)
+            for x in range(h_count):
+                h = Atom(1)
+                molecule.atoms.append(h)
+                bond_h = Bond(order=1, atom1=atom, atom2=h)
+                molecule.bonds.append(bond_h)
 
-                            for number in numbers_list:
-                                if not number in numbering_stack:
-                                    numbering_stack[number] = atom
-                                else:
-                                    numbering_bond = Bond(order=1, cis_trans=cis_trans_sign)
-                                    numbering_bond.add_atom(numbering_stack[number])
-                                    numbering_bond.add_atom(atom)
-                                    molecule.bonds.append(numbering_bond)
-                                    del(numbering_stack[number])
-
-                        else:
-                            tail = bond_expression
-
-                        if not tail:
-                            bond = Bond(order=1)
-                        else:
-                            t = self.TAIL.match(tail).groupdict()
-                            branch, cis_trans_sign, bond_type = t['branch'], t['cis_trans_sign'], t['bond_type']
-                            if branch:
-                                if branch == '(':
-                                    atom_stack.append(atom)
-                                elif branch == ')':
-                                    after_branch_close = True
-                                elif branch == ')(':
-                                    after_branch_close_open = True
-                                else:
-                                    print branch
-                            cis_trans_sign = cis_trans_sign or None
-                            if not bond_type:
-                                bond = Bond(order=1, cis_trans=cis_trans_sign)
-                            elif bond_type == '=':
-                                bond = Bond(order=2, cis_trans=cis_trans_sign)
-                            elif bond_type == '#':
-                                bond = Bond(order=3, cis_trans=cis_trans_sign)
-                            elif bond_type == '.':
-                                # ionic character of bond
-                                bond = Bond(order=1, cis_trans=cis_trans_sign)
-
-                    previous_bond = bond
-
+            if previous_atom:
+                previous_bond.add_atom(atom)
+                if after_branch_close:
+                    atom_pop = atom_stack.pop()
+                    previous_bond.add_atom(atom_pop)
+                    after_branch_close = False
+                if after_branch_close_open:
+                    previous_bond.add_atom(atom_stack[-1])
+                    after_branch_close_open = False
                 else:
-                    index += 1
+                    previous_bond.add_atom(previous_atom)
+                molecule.bonds.append(previous_bond)
+            previous_atom = atom
+            if not bond_exp:
+                bond = Bond(order=1)
+            else:
+                split_bond_expression = self.BREAKING_BOND.match(bond_exp)
+                if split_bond_expression:
+                    split_bond_expression = split_bond_expression.groupdict()
+                    breaking_bond = split_bond_expression['breaking_bond']
+                    tail = split_bond_expression['tail']
+                    if not breaking_bond[0].isdigit() and breaking_bond[0] != '%':
+                        cis_trans_sign, numbers = breaking_bond[0], breaking_bond[1:]
+                    else:
+                        cis_trans_sign, numbers = None, breaking_bond
+                    numbers_list = []
+                    while numbers:
+                        if not numbers[0] == '%':
+                            number, numbers = numbers[0], numbers[1:]
+                        else:
+                            number, numbers = numbers[1:3], numbers[3:]
+                        numbers_list.append(number)
+
+                    for number in numbers_list:
+                        if not number in numbering_stack:
+                            numbering_stack[number] = atom
+                        else:
+                            numbering_bond = Bond(order=1, cis_trans=cis_trans_sign, atom1=numbering_stack[number], atom2=atom)
+                            molecule.bonds.append(numbering_bond)
+                            del(numbering_stack[number])
+                else:
+                    tail = bond_exp
+                if not tail:
+                    bond = Bond(order=1)
+                else:
+                    t = self.TAIL.match(tail).groupdict()
+                    branch, cis_trans_sign, bond_type = t['branch'], t['cis_trans_sign'], t['bond_type']
+                    if branch:
+                        if branch == '(':
+                            atom_stack.append(atom)
+                        elif branch == ')':
+                            after_branch_close = True
+                        elif branch == ')(':
+                            after_branch_close_open = True
+                        cis_trans_sign = cis_trans_sign or None
+                    if not bond_type:
+                        bond = Bond(order=1, cis_trans=cis_trans_sign)
+                    elif bond_type == '=':
+                        bond = Bond(order=2, cis_trans=cis_trans_sign)
+                    elif bond_type == '#':
+                        bond = Bond(order=3, cis_trans=cis_trans_sign)
+                    elif bond_type == '.':
+                        # ionic character of bond
+                        bond = Bond(order=1, cis_trans=cis_trans_sign)
+
+            previous_bond = bond
+
+
         # add atoms h on unocuppy atoms
         free_atoms = {}
         for index, atom in enumerate(molecule.atoms):
+            #print atom
             if atom.bracket:
                 continue
             aromatic = 1 if atom.aromatic else 0
@@ -247,6 +229,11 @@ class SmilesParser(Parser):
                 b.add_atom(h)
                 b.add_atom(atom)
 
+
+
+
+
+
         return molecule
 
 
@@ -264,4 +251,23 @@ if __name__ == '__main__':
     smiles = open(os.getcwd() + '/smiles.txt', 'r')
     for smile in smiles:
         mol = p.decode(smile.split(' ')[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
